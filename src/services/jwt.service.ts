646e1a3ba54e1ba443f635bb27a52414586255b2
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2023-04-12 12:50:17 +0800

    updated schema for user jwt blacklist

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,8 +1,14 @@
 import jwt from "jsonwebtoken";
+import PrismaService from "./prisma.service";
 import { env } from "./../config/app.config";
 
 export default class JWTService {
 	private JWT_KEY: string = env("JWT_SECRET_KEY");
+	private prismaService: PrismaService;
+
+	constructor() {
+		this.prismaService = new PrismaService();
+	}
 
 	public createAccessToken = (payload: any): string => {
 		return jwt.sign(payload, this.JWT_KEY, {
@@ -10,8 +16,16 @@ export default class JWTService {
 		});
 	};
 
-	public revokeAccessToken = (authToken: string) => {
-		//
+	public revokeAccessToken = async (authToken: string) => {
+		const revokedToken = await this.prismaService.prisma.userJWTBlacklist.create({
+			data: {
+				token: authToken,
+			},
+		});
+
+		return {
+			isLogoutSuccess: revokedToken ? true : false,
+		};
 	};
 
 	public verifyAccessToken = (authToken: string): any => {
@@ -24,11 +38,20 @@ export default class JWTService {
 
 			return {
 				isValid: true,
+				data: {
+					user,
+				},
 			};
 		});
 	};
 
 	public decodeAccessToken = (authToken: string): any => {
-		//
+		const user = this.verifyAccessToken(authToken);
+
+		if (user.isValid) {
+			return user.data.user;
+		}
+
+		return null;
 	};
 }

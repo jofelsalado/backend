830da28c61c9e91cfcfef572fd16e804b4a7acb4
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2023-04-14 10:17:09 +0800

    fixed seeder.ts file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,37 +4,37 @@ import { hashPassword } from "./../src/utilities/password.util";
 const prismaService = new PrismaService();
 
 async function main() {
-	// await prismaService.prisma.userType.createMany({
-	// 	data: [
-	// 		{
-	// 			name: "Admin",
-	// 		},
-	// 		{
-	// 			name: "Leads",
-	// 		},
-	// 		{
-	// 			name: "Advisor",
-	// 		},
-	// 	],
-	// });
+	await prismaService.prisma.userType.createMany({
+		data: [
+			{
+				name: "Admin",
+			},
+			{
+				name: "Leads",
+			},
+			{
+				name: "Advisor",
+			},
+		],
+	});
 
-	// await prismaService.prisma.user.create({
-	// 	data: {
-	// 		accountNo: "ACCT041120231549",
-	// 		avatarImage: "NA",
-	// 		firstName: "Admin",
-	// 		middleName: "",
-	// 		lastName: "Account",
-	// 		email: "admin@allcarebusiness.com",
-	// 		username: "admin2023",
-	// 		password: await hashPassword("admin2023"),
-	// 		gender: "NA",
-	// 		address: "NA",
-	// 		birthdate: "NA",
-	// 		contactNo: "NA",
-	// 		userTypeId: 1,
-	// 	},
-	// });
+	await prismaService.prisma.user.create({
+		data: {
+			accountNo: "ACCT041120231549",
+			avatarImage: "NA",
+			firstName: "Admin",
+			middleName: "",
+			lastName: "Account",
+			email: "admin@allcarebusiness.com",
+			username: "admin2023",
+			password: await hashPassword("admin2023"),
+			gender: "NA",
+			address: "NA",
+			birthdate: "NA",
+			contactNo: "NA",
+			userTypeId: 1,
+		},
+	});
 
 	await prismaService.prisma.user.create({
 		data: {
